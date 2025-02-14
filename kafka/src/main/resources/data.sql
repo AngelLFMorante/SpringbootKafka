fa5fcc9660db27dd97c75795247ab9a619500213
Angel Fm <angellfmorante@gmail.com>
2025-02-14 11:20:57 +0100

    kafka

--- kafka/src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-// SQL inicial (resources/data.sql)
-INSERT INTO employee (name, position) VALUES ('Alice', 'Engineer');
-INSERT INTO employee (name, position) VALUES ('Bob', 'Manager');
-INSERT INTO employee (name, position) VALUES ('Charlie', 'HR');
-INSERT INTO employee (name, position) VALUES ('David', 'Developer');
-INSERT INTO employee (name, position) VALUES ('Eve', 'Designer');
-INSERT INTO employee (name, position) VALUES ('Frank', 'Analyst');
